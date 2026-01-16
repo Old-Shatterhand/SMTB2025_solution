3d65b969379f14c06635b084bddb94c981b8e9f9
Old-Shatterhand <joeres.roman@web.de>
2026-01-16 10:31:26 +0100

    Missing install statement added

--- chr_aa_pred_1.sh
+++ chr_aa_pred_1.sh
@@ -4,6 +4,8 @@ NCORES=12
 echo "Go Home"
 cd $HOME/SMTB2025_solution
 
+conda run -n plm --no-capture-output pip install --extra-index-url=https://pypi.nvidia.com "cudf-cu12==25.12.*" "dask-cudf-cu12==25.12.*" "cuml-cu12==25.12.*" "cugraph-cu12==25.12.*" "nx-cugraph-cu12==25.12.*" "cuxfilter-cu12==25.12.*" "cucim-cu12==25.12.*" "pylibraft-cu12==25.12.*" "raft-dask-cu12==25.12.*" "cuvs-cu12==25.12.*" "nx-cugraph-cu12==25.12.*"
+
 {
     for num in 6 12 30 33; do
         echo "conda run -n plm --no-capture-output python -m src.downstream.aa_cuml --data-path $BASE/datasets/binding.csv --embed-base $BASE/aa_embeddings/esm_t$num/binding/ --n-classes 2 --max-layer $num"
