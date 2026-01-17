0417e9a6373c5d32b21e4d3623dd9d1f01b94298
Old-Shatterhand <joeres.roman@web.de>
2026-01-17 01:08:22 +0100

    Preparation for AA-level predictions

--- chr_aa_pred_2.sh
+++ chr_aa_pred_2.sh
@@ -4,8 +4,6 @@ NCORES=6
 echo "Go Home"
 cd $HOME/SMTB2025_solution
 
-conda run -n plm --no-capture-output pip install --extra-index-url=https://pypi.nvidia.com "cudf-cu12==25.12.*" "dask-cudf-cu12==25.12.*" "cuml-cu12==25.12.*" "cugraph-cu12==25.12.*" "nx-cugraph-cu12==25.12.*" "cuxfilter-cu12==25.12.*" "cucim-cu12==25.12.*" "pylibraft-cu12==25.12.*" "raft-dask-cu12==25.12.*" "cuvs-cu12==25.12.*" "nx-cugraph-cu12==25.12.*"
-
 {
     echo "conda run -n plm --no-capture-output python -m src.downstream.aa_cuml --data-path $BASE/datasets/binding.csv --embed-base $BASE/aa_embeddings/esm_t36num/binding/ --n-classes 2 --max-layer $num"
     echo "conda run -n plm --no-capture-output python -m src.downstream.aa_cuml --data-path $BASE/datasets/scope_40_208.csv --embed-base $BASE/aa_embeddings/esm_t36num/scope_40_208/ --n-classes 3 --max-layer $num"
